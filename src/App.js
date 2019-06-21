b5f1d3dbcce4fa74f316c2aa41f9b395f1f57245
Labais <reinitto@gmail.com>
2019-06-21 19:01:01 +0200

    add bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Congrats from './Congrats';
 import GuessedWords from './GuessedWords';
 
+const guessedWords = [
+  {
+    guessedWord: 'train',
+    letterMatchCount: 3
+  },
+  {
+    guessedWord: 'agile',
+    letterMatchCount: 1
+  },
+  {
+    guessedWord: 'party',
+    letterMatchCount: 5
+  }
+];
+
 function App() {
   return (
-    <div className='App'>
-      <Congrats />
-      <GuessedWords />
+    <div className='container'>
+      <h1>Guess that word!</h1>
+      <Congrats success={true} />
+      <GuessedWords guessedWords={guessedWords} />
     </div>
   );
 }

c9a1c6a11a7a13c2c1296cda798010eafdacbc62
Labais <reinitto@gmail.com>
2019-06-23 02:23:19 +0200

    Add secretWord action and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import { getLetterMatchCount } from '../helpers';
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
-  GUESS_WORD: 'GUESS_WORD'
+  GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD'
 };
 /**
  * Returns Redux Thunk function that dispatched GUESS_WORD action and conditionally CORRECT_GUESS action
@@ -25,5 +27,16 @@ export const guessWord = guessedWord => {
         type: actionTypes.CORRECT_GUESS
       });
     }
+  };
+};
+
+export const getSecretWord = () => {
+  return dispatch => {
+    return axios.get('http://localhost:3030').then(res => {
+      dispatch({
+        type: actionTypes.SET_SECRET_WORD,
+        payload: res.data
+      });
+    });
   };
 };
